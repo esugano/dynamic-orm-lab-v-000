cb3d01acff63e3d90fa354599a48419e4f3a6470
Emily S <emily.sugano@gmail.com>
2018-06-16 21:32:24 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,15 +3,24 @@ require 'active_support/inflector'
 
 class InteractiveRecord
 
+  def self.table_name
+    #creates a table with the name of the class - downcase puralized
+    self.to_s.downcase.pluralize
+  end
+
   def self.column_names
     #return hash instead of a nested array
     DB[:conn].execute.results_as_hash = true
+    #grabs table info
     sql = "pragma table_info('#{table_name}')"
     table_info = DB[:conn].execute(sql)
+    #creates column_names
     column_names = []
+    #goes through array of hashes to just grab the column name
     table_info.each do |row|
       column_names << row["name"]
     end
+    #remove any columns that = nil 
     column_names.compact
   end
 end
