d77219c534eb74266a2b03753cf7d680be79884a
Emily S <emily.sugano@gmail.com>
2018-06-16 21:36:01 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -23,4 +23,8 @@ class InteractiveRecord
     #remove any columns that = nil
     column_names.compact
   end
+
+    self.column_names.each do |col_name|
+      attr_accessor col_name.to_sym
+    end
 end
